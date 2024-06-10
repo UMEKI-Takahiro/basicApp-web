e056b34ac258dab1b5ce6d47509f1b682605df35
梅木崇浩 <forprogramming.b1@gmail.com>
2024-06-10 09:52:26 +0900

    dkc

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
 
   useEffect(() => {
     (async() => {
+      const backendHost = "http://localhost";
       // const backendHost = process.env.BACKEND_HOST;
       // console.log(backendHost);
-      const response = await fetch("http://localhost:4001");
+      const response = await fetch(`${backendHost}/api`);
       setInfo(await response.text());
     })()
   }, []);
